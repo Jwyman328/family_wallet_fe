e5147cf3ed7248165c330fbbfa60994e28755df7
Joseph Wyman <jwymandev@gmail.com>
2024-02-18 15:10:09 -0500

    add initial utxo related requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
 import { ApiClient } from "./api/api";
 
+// use a component libary for ui components
 function App() {
   const getBalance = async () => {
     const handleError = () => {
       console.log("error");
     };
     const response = await ApiClient.getBalance(handleError);
-    console.log("right before setting",response.total);
     setCurrentBalance(response.total);
-    console.log(response);
+  };
+  const getUxtos = async () => {
+    const handleError = () => {
+      console.log("error");
+    };
+    const response = await ApiClient.getUtxos(handleError);
+  };
+  const getUxtoFees = async () => {
+    const handleError = () => {
+      console.log("error");
+    };
+    const response = await ApiClient.getUtxoFee(handleError);
   };
   const [currentBalance, setCurrentBalance] = useState(0);
   return (
     <div className="mt-4">
       <div className="mt-4 text-red-400"> Welcome to the family wallet </div>
-      <button onClick={getBalance}>Click me</button>
+      <button onClick={getBalance}>get utxo balance</button>
       <p>{currentBalance} sats</p>
+
+      <button onClick={getUxtos}>get utxos</button>
+      <button onClick={getUxtoFees}>get utxo fees</button>
     </div>
   );
 }
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,4 @@
+// TODO use react query
 export class ApiClient {
   static async getBalance(onError: () => void) {
     try {
@@ -8,8 +9,39 @@ export class ApiClient {
       }
 
       const data = await response.json();
-      console.log("right after awaiting",data);
-      return data
+      console.log("right after awaiting", data);
+      return data;
+    } catch (error) {
+      onError();
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  }
+  static async getUtxos(onError: () => void) {
+    try {
+      const response = await fetch("http://localhost:5011/utxos");
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      onError();
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  }
+  static async getUtxoFee(onError: () => void) {
+    try {
+      const response = await fetch("http://localhost:5011/utxos/fees");
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      return data;
     } catch (error) {
       onError();
       console.error("There was a problem with the fetch operation:", error);
