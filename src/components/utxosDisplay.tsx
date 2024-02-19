8b05a517cf8039fa5891682f030663b5ed00ffc2
Joseph Wyman <jwymandev@gmail.com>
2024-02-19 13:55:14 -0500

    display unspendable message if unspendable

--- src/components/utxosDisplay.tsx
+++ src/components/utxosDisplay.tsx
@@ -11,26 +11,33 @@ export const UtxosDisplay = ({ utxos, feeRate }: UtxosDisplayProps) => {
     const getUtxoFeeQueryRequest = useGetUtxoFee(utxo.txid, utxo.vout, feeRate);
     const fee = getUtxoFeeQueryRequest?.data?.fee;
     const percentOfTxFee = fee && utxo?.amount ? (fee / utxo.amount) * 100 : 0;
+    const isSpendable: boolean = getUtxoFeeQueryRequest?.data?.spendable;
     return (
       <div>
-        <p className="text-red-600">txid: {utxo.txid}</p>
-        <p className="text-red-600">txvout: {utxo.vout} </p>
-        <p className="text-red-600">
+        <p className="text-black">txid: {utxo.txid}</p>
+        <p className="text-black">txvout: {utxo.vout} </p>
+        <p className="text-black">
           amount: {(utxo.amount / 100000000).toFixed(8)}
         </p>
-        <p className="text-red-600">
-          fee estimate: {(fee / 100000000).toFixed(8) || "pending"}
-        </p>
 
-        <p className="text-red-600">
-          % of tx fee: {percentOfTxFee.toFixed(2)}%
-        </p>
+        {isSpendable ? (
+          <div>
+            <p className="text-black">
+              fee estimate: {(fee / 100000000).toFixed(8) || "pending"}
+            </p>
+            <p className="text-black">
+              % of tx fee: {percentOfTxFee.toFixed(5).replace(/\.?0+$/, "")}%
+            </p>
+          </div>
+        ) : (
+          <p className="text-red-600">not spendable</p>
+        )}
       </div>
     );
   };
   return (
     <div>
-      <p className="text-red-600">utxos display</p>
+      <p className="text-blue-600">utxos display</p>
       {utxos.map((utxo) => {
         return UtxoDisplay(utxo);
       })}
